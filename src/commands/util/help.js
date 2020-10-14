c1264b33436962aa0c5fb30934f01d4a09ffac6f
ItzColtz <65344522+ItzColtz@users.noreply.github.com>
2020-10-14 18:36:53 +0100

    added ability to get more info on a command using ?help [command name]

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -10,21 +10,42 @@ module.exports = class extends Command {
         });
     }
 
-    async execute(message) {
+    async execute(message, args) {
         const embed = {
             color: 'BLUE',
             fields: [],
         };
 
-        const categories = this.client.utils.removeDuplicates(this.client.commands.map(c => c.category));
+        const command = this.client.commands.get(args[0]);
 
-        for (const category of categories) {
-            embed.fields.push({
-                name: `❯ ${this.client.utils.capitalise(category)}`,
-                value: this.client.commands.filter(c => c.category === category).map(c => `\`${c.name}\``).join(' '),
-            });
+        if (command) {
+            embed.title = `\`${command.name} ${command.usage || ''}\``;
+
+            embed.fields.push(
+                {
+                    name: '❯ Description',
+                    value: command.description,
+                },
+            );
+
+            if (command.aliases.length) {
+                embed.fields.push({
+                    name: '❯ Aliases',
+                    value: command.aliases.map(alias => `\`${alias}\``).join(' '),
+                    inline: true,
+                });
+            }
         }
+        else {
+            const categories = this.client.utils.removeDuplicates(this.client.commands.map(c => c.category));
 
+            for (const category of categories) {
+                embed.fields.push({
+                    name: `❯ ${this.client.utils.capitalise(category)}`,
+                    value: this.client.commands.filter(c => c.category === category).map(c => `\`${c.name}\``).join(' '),
+                });
+            }
+        }
 
         message.channel.send({ embed: embed });
     }
