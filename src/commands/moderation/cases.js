6018f0cfa8b70137409546e57382028a046f8af6
ItzColtz <65344522+ItzColtz@users.noreply.github.com>
2020-10-15 11:17:22 +0100

    basics of the case command

---
+++ src/commands/moderation/cases.js
@@ -0,0 +1,41 @@
+const Command = require('../../structures/bases/command');
+const { caseModel } = require('../../database/models/export/index');
+
+const userRegex = RegExp(/<@!?(\d{17,19})>/);
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            name: 'cases',
+            description: 'View all the cases of a member!',
+            category: 'Moderation',
+            usage: '<member>',
+            examples: [
+                'cases @Coltz',
+                'cases 611466971371929602',
+            ],
+        });
+    }
+
+    async execute(message, args) {
+        const userID = userRegex.exec(args[0]) ? userRegex.exec(args[0])[1] : args[0];
+        const user = await this.client.users.fetch(userID).catch(() => message.author);
+
+        const caseDoc = await caseModel.find({
+            guildID: message.guild.id,
+            committerID: user.id,
+        });
+
+        if (!caseDoc.length) {
+            return message.channel.send(`${user.tag} dosn't have any cases.`);
+        }
+
+        const embed = {
+            color: 'BLUE',
+            description: caseDoc.map(Case => `[${Case.case}](${Case.messageLink})`).join('\n'),
+        };
+
+        message.channel.send({ embed: embed });
+    }
+}; 
